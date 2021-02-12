792eed4c8c6860a8aead25b47f3a67e9d797e614
Sergey <ddpsm@ya.ru>
2021-02-13 00:51:17 +0300

    upd task 2

---
+++ folders_gen.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+if [ "$*" = "" ]
+then
+  echo ! Use \$0 args ... 
+  TEXT="no args"
+else 
+  argsi=$*
+  TEXT="argsi $argsi"
+fi
+
+dir=abc
+
+for k in {2010..2017}
+  do
+    mkdir $k
+    for j in {1..12}
+      do
+        mkdir ./$k/$j
+        for i in {1..7}
+        do
+          echo "creating /$k/$j/$dir$i"
+          touch ./$k/$j/$dir$i.txt
+          echo "file created ./$k/$j/$dir$i" > ./$k/$j/$dir$i.txt
+        done
+      done
+  done
+
+echo $TEXT
